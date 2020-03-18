7aa908d3ded54f94583021d706a75d56854fb470
Gavin Zhu <gavinzhu1127@gmail.com>
2020-03-18 10:43:18 -0400

    initial commit

---
+++ auto ML.R
@@ -0,0 +1,13 @@
+library(h2o)
+h2o.init()
+# http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv
+# https://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/kdd2009/small-churn/kdd_train.csv
+# https://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/kdd2009/small-churn/kdd_valid.csv
+
+library(rattle)
+rattle()
+
+
+library(esquisse)
+# esquisser()
+esquisser(iris)
